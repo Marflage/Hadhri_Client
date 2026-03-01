dde217fd74711cf04e8d9e3f72940cae5d501bf0
marflage <istabraq447@gmail.com>
2026-03-01 21:20:44 +0500

    feat: Add option to pull to refresh data in case of connection error

--- lib/features/account/page.dart
+++ lib/features/account/page.dart
@@ -1,10 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:hadhri/features/account/account_form.dart';
 import 'package:hadhri/infrastructure/responses/get_course_plans.dart';
 import 'package:hadhri/infrastructure/services/account_form_service.dart';
-import 'package:http/http.dart';
+import 'package:pullex/pullex.dart';
 
 import '../../infrastructure/services/course_plan_service.dart';
 
@@ -23,29 +21,66 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   late GetCoursePlansResponse _getCoursePlans;
   var _isLoading = true;
+  var _isDataAvailable = false;
+
+  final _refreshController = RefreshController();
 
   @override
   void initState() {
     super.initState();
-    // TODO: Should the operation be awaited here?
-    final vm = widget.service.fetchCoursePlansAsync();
-    vm.then(
-      (value) => setState(() {
-        _isLoading = false;
-        _getCoursePlans = value.data;
-      }),
-    );
+    _onRefresh();
   }
 
   @override
   Widget build(BuildContext context) {
+    // TODO: Analyze if this Scaffold can be removed safely.
     return Scaffold(
-      body: _isLoading
-          ? Center(child: const CircularProgressIndicator.adaptive())
-          : AccountForm(
-              getCoursePlans: _getCoursePlans,
-              service: AccountFormService(),
-            ),
+      body: PullexRefresh(
+        controller: _refreshController,
+        header: BaseHeader(),
+        onRefresh: _onRefresh,
+        child: _isLoading
+            ? Center(child: const CircularProgressIndicator.adaptive())
+            : _isDataAvailable
+            ? AccountForm(
+                getCoursePlans: _getCoursePlans,
+                service: AccountFormService(),
+              )
+            : Center(
+                // child: Text('Pull down to refresh.'),
+                // TODO: Remove this after testing.
+                child: ElevatedButton(onPressed: _onRefresh, child: Text('Refresh')),
+              ),
+      ),
     );
+  }
+
+  Future _onRefresh() async {
+    final vm = await widget.service.fetchCoursePlansAsync();
+
+    setState(() => _isLoading = false);
+
+    if (vm.data == null) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(vm.message)));
+      }
+
+      _refreshController.refreshCompleted();
+
+      return;
+    }
+
+    setState(() {
+      _isDataAvailable = true;
+      _getCoursePlans = vm.data!;
+    });
+
+    _refreshController.refreshCompleted();
+  }
+
+  @override
+  void dispose() {
+    _refreshController.dispose();
+    super.dispose();
   }
 }
