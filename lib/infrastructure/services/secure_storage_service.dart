f340d384ef8c7073b6d9e89b997a92eadbb928b5
marflage <istabraq447@gmail.com>
2026-03-05 22:37:45 +0500

    feat: Create SecureStorageService

---
+++ lib/infrastructure/services/secure_storage_service.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageService {
+  // TODO: Analyze if this should be injected instead of being newed up here.
+  final _storage = FlutterSecureStorage();
+
+  Future<String> read(String key) async {
+    if (key.isEmpty) {
+      throw Exception('Key is invalid');
+    }
+
+    String? value = await _storage.read(key: key);
+
+    if (value == null || value.isEmpty) {
+      throw Exception('No value found for key: $key');
+    }
+
+    return value;
+  }
+
+  Future<void> write(String key, String value) async {
+    if (key.isEmpty) {
+      throw Exception('Key is invalid');
+    }
+
+    if (value.isEmpty) {
+      throw Exception('Value is invalid');
+    }
+
+    await _storage.write(key: key, value: value);
+  }
+
+  Future<void> delete(String key) async {
+    if (key.isEmpty) {
+      throw Exception('Key is invalid');
+    }
+
+    await _storage.delete(key: key);
+  }
+}
