c5b57b0dc14d35e925c12111bf5cdb592f8cd168
marflage <istabraq447@gmail.com>
2026-03-07 14:13:49 +0500

    feat: Create DiContainer

---
+++ lib/infrastructure/di_container.dart
@@ -0,0 +1,23 @@
+import 'package:hadhri/infrastructure/services/attendance_service.dart';
+import 'package:hadhri/infrastructure/services/auth_service.dart';
+import 'package:hadhri/infrastructure/services/course_plan_service.dart';
+import 'package:hadhri/infrastructure/services/secure_storage_service.dart';
+import 'package:hadhri/infrastructure/utils/api_client.dart';
+
+class DiContainer {
+  static late final SecureStorageService storageService;
+  static late final AuthService authService;
+  static late final ApiClient apiClient;
+
+  static late final AttendanceService attendanceService;
+  static late final CoursePlanService coursePlanService;
+
+  static void init() {
+    storageService = SecureStorageService();
+    authService = AuthService(storageService: storageService);
+    apiClient = ApiClient(authService: authService);
+
+    attendanceService = AttendanceService(apiClient: apiClient);
+    coursePlanService = CoursePlanService();
+  }
+}
